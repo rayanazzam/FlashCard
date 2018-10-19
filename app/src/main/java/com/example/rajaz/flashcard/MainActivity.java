a68a7339fceadd2dd677186882c54e0db78c6b8e
rayanazzam <you@example.com>
2018-10-18 21:27:47 -0500

    Tried implementing optional features. Additional work required

--- app/src/main/java/com/example/rajaz/flashcard/MainActivity.java
+++ app/src/main/java/com/example/rajaz/flashcard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rajaz.flashcard;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,35 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
 
             }
+        });
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+
+            }
+        });
+        findViewById(R.id.option_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_one).setBackgroundColor(Color.GREEN);
+            }
+
+        });
+        findViewById(R.id.option_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_two).setBackgroundColor(Color.GREEN);
+            }
+
+        });
+        findViewById(R.id.option_three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_three).setBackgroundColor(Color.GREEN);
+            }
+
         });
     }
 }
