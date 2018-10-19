f040457a701bdc67e40d2189a28889713e15edfd
Rayan Ahmed <rahmed@ltsp19.udallas.edu>
2018-10-19 05:40:02 -0500

    Made updates to optional features. Revise once more before submitting project.

--- app/src/main/java/com/example/rajaz/flashcard/MainActivity.java
+++ app/src/main/java/com/example/rajaz/flashcard/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.rajaz.flashcard;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +31,49 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.option_one).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.option_one).setBackgroundColor(Color.GREEN);
+                findViewById(R.id.option_one).setBackgroundColor(getResources().getColor(R.color.darkBlue));
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.white));
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.white));
+
             }
 
+        });findViewById(R.id.show_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //((ImageView) findViewById(R.id.hide_icon)).setImageResource(R.drawable.ic_iconmonstr_eye_5);
+                findViewById(R.id.option_one).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_two).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_three).setVisibility(View.INVISIBLE);
+                findViewById(R.id.hide_icon).setVisibility(View.VISIBLE);
+                findViewById(R.id.show_icon).setVisibility(View.INVISIBLE);
+            }
+        });
+        findViewById(R.id.hide_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.show_icon).setVisibility(View.VISIBLE);
+                findViewById(R.id.hide_icon).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_one).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_two).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_three).setVisibility(View.VISIBLE);
+
+            }
         });
         findViewById(R.id.option_two).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.option_two).setBackgroundColor(Color.GREEN);
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.darkBlue));
+                findViewById(R.id.option_one).setBackgroundColor(getResources().getColor(R.color.white));
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.white));
             }
 
         });
         findViewById(R.id.option_three).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.option_three).setBackgroundColor(Color.GREEN);
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.darkBlue));
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.white));
+                findViewById(R.id.option_one).setBackgroundColor(getResources().getColor(R.color.white));
             }
 
         });
